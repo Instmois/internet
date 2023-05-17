0da8b0c00ed117aecaf0c7c4fa876d5c73116c32
nikita <nikita-moiseev-2003@ya.ru>
2023-05-17 16:03:52 +0300

    2

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -1,7 +1,11 @@
 package com.example.springboot;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.jsoup.Jsoup;
 
 @SpringBootApplication
 @RestController
@@ -28,8 +33,12 @@ public class Application {
 		return String.format("Hello %s!", name);
 	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	public static void main(String[] args) throws IOException {
+        Document document = Jsoup.connect("https://ru.investing.com/equities/").get();
+		Elements h1 = document.select("tbody");
+		System.out.println(h1);
+        SpringApplication.run(Application.class, args);
+
 	}
 
 	@Bean
